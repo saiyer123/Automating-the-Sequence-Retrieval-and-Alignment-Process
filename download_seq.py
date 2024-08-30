c6944b28b1561414e35c970f4fb171ffaae4840f
saiyer123 <130099176+saiyer123@users.noreply.github.com>
2024-08-29 23:41:36 -0700

    Update download_seq.py

--- download_seq.py
+++ download_seq.py
@@ -70,6 +70,16 @@ def AT_content(output_file):
     print(f"AT Percentage = {at_percentage:.2f}%")
     return at_percentage
 
+# Function to calculate nucleotide length
+def nuc_length(output_file):
+    atcg_length = 0
+    for nuc in output_file:
+        if nuc in 'ATCG':
+            atcg_length += 1
+    print("f{atcg_length}")
+    
+
+
 
             
         
@@ -91,6 +101,9 @@ if __name__ == "__main__":
 
     # Add headers to output file
     add_headers_with_format(blast_output)
+
+    # Calculate nucleotide length
+    nuc_length(blast_output)
 
 
 #To create this code, I first imported Entrez using biopython. This gives me access to the NCBI database.
