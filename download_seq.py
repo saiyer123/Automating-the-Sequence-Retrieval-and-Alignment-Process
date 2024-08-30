bd8c47029258b55871112696a6f2f24c138b9b69
saiyer123 <130099176+saiyer123@users.noreply.github.com>
2024-08-29 22:42:43 -0700

    Update download_seq.py

--- download_seq.py
+++ download_seq.py
@@ -38,18 +38,18 @@ if __name__ == "__main__":
     # Run BLAST on the fetched sequence
     run_blast(fasta_file, blast_output)
 
-<!-- 
-To create this code, I first imported Entrez using biopython. This gives me access to the NCBI database.
-Eventually we want to run blast on our dataset so we need to import subprocess for that. In fetching our DNA
-sequence I created a function for that takes 2 parameters: gene_id and output_file. The gene_id is the specific 
-gene you want to obtain and the output file is where your sequence will be stored. Then we specify with queries the 
-information needed. We are searching the nucleotide database from the gene id we provide, then the returning it as a fasta file 
-and as text rather than binary. Once we get all the information we close the NCBI server. Then we just print a message
-saying we the fetched sequence was saved in the output file. 
-    Our Blast function takes 2 parameters: An input file and an output file. Subprocess.run is used to run the blastn command. 
-Just like the previous function all the paramaters are being listed inside subpress, and then the results are saved to an output file.
-If there is an error running blast it will display that there is an error.
-    Our Main script is testing this on the given gene id: NM_007294. The fasta file is the name where the sequence will be saved.
-The the blast output is where the blast results will be saved. We finall call our previous 2 functions and obtain our sequence, then run
-blast on it and we will obtain 2 new files with the outputs.
--->
+
+#To create this code, I first imported Entrez using biopython. This gives me access to the NCBI database.
+#Eventually we want to run blast on our dataset so we need to import subprocess for that. In fetching our DNA
+#sequence I created a function for that takes 2 parameters: gene_id and output_file. The gene_id is the specific 
+#gene you want to obtain and the output file is where your sequence will be stored. Then we specify with queries the 
+#information needed. We are searching the nucleotide database from the gene id we provide, then the returning it as a fasta file 
+#and as text rather than binary. Once we get all the information we close the NCBI server. Then we just print a message
+#saying we the fetched sequence was saved in the output file. 
+#    Our Blast function takes 2 parameters: An input file and an output file. Subprocess.run is used to run the blastn command. 
+#Just like the previous function all the paramaters are being listed inside subpress, and then the results are saved to an output file.
+#If there is an error running blast it will display that there is an error.
+#    Our Main script is testing this on the given gene id: NM_007294. The fasta file is the name where the sequence will be saved.
+#The the blast output is where the blast results will be saved. We finall call our previous 2 functions and obtain our sequence, then run
+#blast on it and we will obtain 2 new files with the outputs.
+
