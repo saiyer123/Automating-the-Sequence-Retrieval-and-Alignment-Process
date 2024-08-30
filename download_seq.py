e3eda9e02c6fdf54951a689e4efd9ec58ddd2f16
saiyer123 <130099176+saiyer123@users.noreply.github.com>
2024-08-30 00:04:18 -0700

    Update download_seq.py

--- download_seq.py
+++ download_seq.py
@@ -55,7 +55,7 @@ def GC_content(output_file):
             if nuc in 'GC':
                 gc_total += 1
     gc_percentage = (gc_total / atcg_length) * 100
-    print("Sequence Length: ") + str(atcg_length) + "Nucleotides"
+    print(f"GC Percentage = {gc_percentage:.2f}%")
     print(f"Nuc length = {atcg_length}")
     return gc_percentage
 
